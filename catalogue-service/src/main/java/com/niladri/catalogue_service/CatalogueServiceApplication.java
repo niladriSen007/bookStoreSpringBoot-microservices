4d335ff8c64d832aaa3a51a656994c1fd8517a80
Niladri <niladrisengit12@gmail.com>
2025-11-09 23:10:40 +0530

    configure application properties and add CI/CD pipeline for catalogue service

--- catalogue-service/src/main/java/com/niladri/catalogue_service/CatalogueServiceApplication.java
+++ catalogue-service/src/main/java/com/niladri/catalogue_service/CatalogueServiceApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CatalogueServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CatalogueServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogueServiceApplication.class, args);
+    }
 }
--- catalogue-service/src/test/java/com/niladri/catalogue_service/TestCatalogueServiceApplication.java
+++ catalogue-service/src/test/java/com/niladri/catalogue_service/TestCatalogueServiceApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 
 public class TestCatalogueServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.from(CatalogueServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.from(CatalogueServiceApplication::main)
+                .with(TestcontainersConfiguration.class)
+                .run(args);
+    }
 }
