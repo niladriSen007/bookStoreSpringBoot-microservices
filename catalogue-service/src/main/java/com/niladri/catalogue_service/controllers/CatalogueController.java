86b0f8d8868232c182845d092ad19ff2e0bda9f3
Niladri <niladrisengit12@gmail.com>
2025-11-09 23:16:51 +0530

    refactor CatalogueController to remove unnecessary blank lines

--- catalogue-service/src/main/java/com/niladri/catalogue_service/controllers/CatalogueController.java
+++ catalogue-service/src/main/java/com/niladri/catalogue_service/controllers/CatalogueController.java
@@ -5,5 +5,4 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/core")
-public class CatalogueController {
-}
+public class CatalogueController {}
